3025fc8e4c49022e50e48151fd89408ea74cc120
dmuraco3 <dylanjmuraco@gmail.com>
2021-10-25 18:16:46 -0400

    yeah aadded a bunch of stuff

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,8 +1,23 @@
 import styles from './cart.module.scss'
+
+import { add, remove, selectCart } from "../../stores/reducers/cart";
+import { useSelector, useDispatch } from "react-redux";
+import { store } from "../../stores/configureStore";
+
 export default function Cart() {
+    const cart = useSelector(selectCart);
     return (
         <div className={styles.main}>
-            
+            <div className={styles.CartContainer}>
+                {cart && cart.map((item, index) => (
+                    <div className={styles.CartItem} key={index}>
+                        {JSON.stringify(item)}
+                    </div>
+                ))}
+            </div>
+            <div className={styles.summary}>
+                
+            </div>
         </div>
     )
 }
